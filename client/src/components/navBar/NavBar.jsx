55926192da742ea5f360e82462bba2b563d8908e
88vane88 <vancig88@gmail.com>
2023-04-13 18:16:31 -0300

    responsive navbar

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,43 +1,52 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import { TotalItemsCart } from '../buy/TotalItemsCart'
 import { useContext } from "react"
 import { dataContext } from "../dataProducts/DataProducts"
 import './NavBar.css'
+import ReorderIcon from '@mui/icons-material/Reorder';
+
+/* import {FaBars, FaTimes} from "react-icons/fa"; */
 
 export const NavBar = () => {
   const {cart, setCart}=useContext(dataContext)
+  const [showLinks, setShowLinks] = useState(false)
 
   return (
-    <div className='nav-container'>
       <nav className='navbar'>
-        <div className='contenedorLogo'>
-          <Link  to={'/'}>
-          <div className='navbar-logo'>Salón Omar</div>
-          <div className='navbar-logo2'>Peluquería</div>
+        <div className='leftSide'>
+          <Link  to={'/'} className= 'logo'>
+          <div >Salón Omar</div>
+          <div >Peluquería</div>
           </Link>
         </div>
-      <div className='link-container'>
-      <a className='about' href= '#about' >Nosotros</a>
-      <a className='maps' href= '#ubi'>Ubicación</a>
-      <Link className='products' to={'/products'}>Productos</Link>
-      <Link className='loginUser' to={'/login'}>Registrarse</Link>
-      <a className='whatsapp' target='_blank' href='https://api.whatsapp.com/send?phone=1568298132&text=BIENVENID@'>
-        <ion-icon name="logo-whatsapp"></ion-icon>
-      </a>
-      <a className='instagram' target='_blank' href='https://www.instagram.com/salon.omar/?hl=es'>
-        <ion-icon name="logo-instagram"></ion-icon>
-      </a>
-      </div>
-      <Link className='seeCarrito' to={'/buy'}>
-        🛒
-        {
-          cart.length >0
-          ? <TotalItemsCart/>
-          :null
-        }
-      </Link>
+        <div className='rightSide'>
+          <div className='linksTo' id={showLinks ? 'hidden': ''}>
+            <a className='about' href= '#about' >Nosotros</a>
+            <a className='maps' href= '#ubi'>Ubicación</a>
+            <Link className='products' to={'/products'}>Productos</Link>
+            <Link className='loginUser' to={'/login'}>Registrarse</Link>
+          </div>
+            <a className='whatsapp' target='_blank' href='https://api.whatsapp.com/send?phone=1568298132&text=BIENVENID@'>
+              <ion-icon name="logo-whatsapp"></ion-icon>
+            </a>
+            <a className='instagram' target='_blank' href='https://www.instagram.com/salon.omar/?hl=es'>
+            <ion-icon name="logo-instagram"></ion-icon>
+            </a>
+            <Link className='seeCarrito' to={'/buy'}>
+            🛒
+          {
+            cart.length >0
+            ? <TotalItemsCart/>
+            :null
+          }
+          </Link>
+        </div>
+        <button className='buttonOpen' onClick={()=>setShowLinks(!showLinks)}>
+         <ReorderIcon/>
+        </button>
       </nav>
-    </div>
+
   )
 }
 
